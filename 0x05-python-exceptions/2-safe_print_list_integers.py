ed71eafa4cbf1493d5b5f00b5833351425d4dd2e
omazoz <oumaymamazoz12@gmail.com>
2023-06-27 01:34:39 +0100

    fixed check 45

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    counter = 0
     for j in range(x):
         try:
             print("{:d}".format(my_list[j]), end="")
@@ -9,4 +9,4 @@ def safe_print_list_integers(my_list=[], x=0):
         else:
             counter += 1
     print()
-    return (i)
+    return (counter)
