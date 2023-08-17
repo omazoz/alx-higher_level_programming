193204fdd4ada216cf27f113e5327ecb2278e383
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2023-08-17 16:44:34 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
+-- This script created by Oumayma Mazoz
 -- script to convert hbtn_0c_0 to utf-8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
