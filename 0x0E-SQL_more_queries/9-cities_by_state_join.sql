406b8590aeb4c18967d29498bc640d90b99a46d8
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2023-08-17 16:55:04 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
+-- This Script Created BY Oumayma Mazoz
 -- script that lists all shows contained in hbtn_0d_tvshows
 SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states ON cities.state_id=states.id
