a17705aeb03849d292648c30ad447173f789c85a
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2023-08-17 16:52:59 +0100

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
+-- This Script Created BY Oumayma Mazoz
 -- script that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- user_0d_1 should have all privileges on your MySQL server
